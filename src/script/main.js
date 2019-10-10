46ce662256be2979c16b664de0cd24bfa046a5ad
Cherkesky <guy@cherkesky.com>
2019-10-10 16:03:05 -0500

    nailed it

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,55 @@
-console.log("working");
+
+
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill (currentObject) {
+  // Modify the food so that it is cooked
+  currentObject.cooked = true;
+
+  // Put the cooked food into the appropriate array
+  cookedFood.push(currentObject);
+};
+
+
+// Your task is to iterate over the array of foods and invoke the function for each item so that the cookedFood array contains all of the items after they are cooked.
+
+
+//verifting that there is no cook food 
+console.log(cookedFood);
+
+//lets cook some food - iterating through the food array calling the cook function
+for (i=0; i<foods.length; i++){
+   grill(foods[i]);
+}
+//verifting that all food has been cook 
+console.log(cookedFood);
